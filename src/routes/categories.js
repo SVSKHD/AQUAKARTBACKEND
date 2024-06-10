b151c99bec737fabf0ee0fa61d6eb588d370e43f
8svskhd@gmail.com <hithesh.sunkara@sportsit.net>
2024-06-11 01:41:42 +0530

    categories

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -6,6 +6,6 @@ const router = express.Router()
 router.get("/allcategories" , categoryFunctions().getAllCategories)
 router.get("/category/:id" , categoryFunctions().getCategory)
 router.get("/category-remove/:id" , categoryFunctions().deleteCategory)
-router.post("/category-add" , ,categoryFunctions().addCategory)
-router.put("/category-update" , ,categoryFunctions().updateCategory)
+router.post("/category-add" , categoryFunctions().addCategory)
+router.put("/category-update"  ,categoryFunctions().updateCategory)
 export default router
