31e7afe721a6c3cb310b9d91144c21a967ab96ea
8svskhd@gmail.com <hithesh.sunkara@sportsit.net>
2024-06-11 13:12:30 +0530

    category

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -4,6 +4,10 @@ import userAuth from "../middleware/user";
 
 const router = express.Router();
 
+router.get("/category-status", (req, res) => {
+  res.json({ message: "Category Status v1 active" });
+});
+
 router.get("/allcategories", categoryFunctions().getAllCategories);
 router.get("/category/:id", categoryFunctions().getCategory);
 router.get("/category-remove/:id", categoryFunctions().deleteCategory);
