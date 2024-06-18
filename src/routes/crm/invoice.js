8d7075ecd3ede3ed1e1510d3a301e41008529282
SVSKHD <8svskhd@gmail.com>
2024-06-18 22:46:48 +0530

    invoice and payment link

--- src/routes/crm/invoice.js
+++ src/routes/crm/invoice.js
@@ -1,9 +1,18 @@
 import express from "express";
+import InvoiceOperations from "../../controllers/crm/invoice.js";
+import userAuth from "../../middleware/user.js";
 
 const router = express.Router();
 
 router.get("invoice-status", async (req, res) => {
   res.json({ message: "Invoice Status v1 active" });
 });
+
+router.get("/all-invoices",InvoiceOperations.getInvoices)
+router.get("/invoice/:id",InvoiceOperations.getInvoice)
+router.get("/invoice/:id", userAuth.checkAdmin , InvoiceOperations.deleteInvoice)
+router.post("/create/invoice", userAuth.checkAdmin , InvoiceOperations.createInvoice)
+router.put("/update/invoice/:id" , userAuth.checkAdmin , InvoiceOperations.updateInvoice)
+
 
 export default router;
