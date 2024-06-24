f82508f33ce5db602efb3d7a9095de200c3a7c36
SVSKHD <8svskhd@gmail.com>
2024-06-24 14:53:07 +0530

    link

--- src/routes/crm/paymentLink.js
+++ src/routes/crm/paymentLink.js
@@ -1,6 +1,6 @@
 import express from "express"
 import PaymentOperations from "../../controllers/crm/paymentLink.js"
-import paymentOperations from "../../controllers/phonepeGateway.js"
+
 
 
 const router = express.Router()
@@ -8,7 +8,7 @@ router.get("/payment-api-link-status", (req,res)=>{
     res.json({"message":"Aquakart v1 status is active"})
 })
 
-router.post("/phonepe/payment-link",paymentOperations.payPhonepe)
+router.post("/phonepe/payment-link",PaymentOperations.payPhonepe)
 
 
 
