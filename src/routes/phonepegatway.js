9619786decf0a75238c87813f1d9b9ddace1ec80
SVSKHD <8svskhd@gmail.com>
2024-08-02 00:02:16 +0530

    gateway security

--- src/routes/phonepegatway.js
+++ src/routes/phonepegatway.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
   res.json({ message: "Phone Pe Status v1 active" });
 });
 
-router.post("/pay-phonepe", paymentOperations.payPhonepe);
+router.post("/pay-phonepe",userAuth.isLoggedIn, paymentOperations.payPhonepe);
 
-router.post("/phonepe-verify/:id", paymentOperations.handlePhoneOrderCheck);
+router.post("/phonepe-verify/:id", userAuth.isLoggedIn, paymentOperations.handlePhoneOrderCheck);
 export default router;
