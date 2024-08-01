0ac79f872c301c6a816653ab37b7eb8c44b93d70
SVSKHD <8svskhd@gmail.com>
2024-08-01 15:35:44 +0530

    check

--- src/routes/phonepegatway.js
+++ src/routes/phonepegatway.js
@@ -11,5 +11,5 @@ router.get("/", async (req, res) => {
 
 router.post("/pay-phonepe", paymentOperations.payPhonepe);
 
-router.get("/phonepe-verify/:id", paymentOperations.handlePhoneOrderCheck);
+router.post("/phonepe-verify/:id", paymentOperations.handlePhoneOrderCheck);
 export default router;
