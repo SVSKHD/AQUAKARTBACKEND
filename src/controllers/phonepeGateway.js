aa47f263c8547b53bb525d5881e95c4817b938df
SVSKHD <8svskhd@gmail.com>
2024-08-01 17:37:41 +0530

    gateway

--- src/controllers/phonepeGateway.js
+++ src/controllers/phonepeGateway.js
@@ -39,7 +39,7 @@ const payPhonepe = async (req, res) => {
       merchantUserId: passedPayload.user,
       name: getUserById.name || createUserName(getUserById.email),
       amount: passedPayload.totalAmount * 100,
-      redirectUrl: `https://api.aquakart.co.in/v1/phonepe-verify/${merchantTransactionId}`,
+      redirectUrl: `https://api.aquakart.co.in/order/processing`,
       redirectMode: "POST",
       callbackUrl: `https://api.aquakart.co.in/v1/phonepe-verify/${merchantTransactionId}`,
       mobileNumber: passedPayload.number,
